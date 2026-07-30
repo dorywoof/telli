5bf2aedce998a3de95140a3ad49cc4e01ecfac6d
Doruk <dorukmiril00@gmail.com>
2026-07-30 14:27:10 +0300

    thumb grace hold: keep detected thumb up through brief dropouts

--- app.js
+++ app.js
@@ -431,15 +431,19 @@ function fingersUp(pts, key) {
   const t1x = pts[2].x - pts[3].x, t1y = pts[2].y - pts[3].y;
   const t2x = pts[4].x - pts[3].x, t2y = pts[4].y - pts[3].y;
   const thumbCos = (t1x * t2x + t1y * t2y) / (Math.hypot(t1x, t1y) * Math.hypot(t2x, t2y) + 1e-6);
+  let rawThumb;
   if (otherUp >= 3) {
-    up.thumb = dist(pts[4], pts[9]) > palm * (st.thumb ? 1.05 : 1.2) &&
+    rawThumb = dist(pts[4], pts[9]) > palm * (st.thumb ? 1.05 : 1.2) &&
       dist(pts[4], pts[5]) > palm * (st.thumb ? 0.55 : 0.66) &&
       thumbCos < (st.thumb ? -0.5 : -0.62);
   } else {
-    up.thumb = dist(pts[4], pts[17]) > dist(pts[3], pts[17]) * (st.thumb ? 1.05 : 1.14) ||
+    rawThumb = dist(pts[4], pts[17]) > dist(pts[3], pts[17]) * (st.thumb ? 1.05 : 1.14) ||
       dist(pts[4], pts[5]) > palm * (st.thumb ? 0.4 : 0.52) ||
       dist(pts[4], pts[9]) > palm * (st.thumb ? 0.88 : 1.0);
   }
+  const tnow = performance.now();
+  if (rawThumb) st.thumbSeenAt = tnow;
+  up.thumb = rawThumb || (st.thumbSeenAt && tnow - st.thumbSeenAt < 160);
   st.thumb = up.thumb;
   return up;
 }
@@ -465,7 +469,7 @@ function analyzeLeft(pts) {
   } else {
     if (tilt > 34) tiltMinor = true;
   }
-  return { degree, tilted: tiltMinor };
+  return { degree, tilted: tiltMinor, up };
 }
 
 function analyzeRight(pts) {
